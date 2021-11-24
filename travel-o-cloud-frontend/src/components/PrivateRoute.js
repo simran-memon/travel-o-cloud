0c2db8c04fc4772a6869142df894fe01bf3d4ab7
Mounica Kamireddy <kamireddym28@gmail.com>
2021-11-23 19:29:23 -0800

    Updated endpoint

--- travel-o-cloud-frontend/src/components/PrivateRoute.js
+++ travel-o-cloud-frontend/src/components/PrivateRoute.js
@@ -8,7 +8,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
     const auth = null; 
     
-    return auth ? <Outlet /> : <Navigate to="/home" />;
+    return auth ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default PrivateRoute;
